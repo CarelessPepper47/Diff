4c3f2f8df18e77f4c9e76e4551eea23ff00d486f
MatiDev <mtsz.raszeja@gmail.com>
2024-02-22 15:35:08 +0100

    Dodatkowe zadanie zrobione w L02.js

--- L02.js
+++ L02.js
@@ -9,3 +9,33 @@ if (password.length >= 6) {
 } else {
     console.log("Za krótkiego masz hasło")
 }
+
+// dodatkowe 
+
+let dzienTygodnia = prompt("Podaj dzień tygodnia", "Poniedziałek")
+
+if (dzienTygodnia === "Poniedziałek") {
+    console.log("Bruh")
+} else if (dzienTygodnia === "Sobota" && "Niedziela") {
+    console.log("To w sumie nawet lepiej")
+} else if (dzienTygodnia !== "Piątek") {
+    console.log("Długo jeszcze?")
+} else {
+    console.log("Jeszcze tylko troszke")
+}
+
+function getColor(phrase){
+    //WRITE YOUR CODE BETWEEN THIS LINE: ↓ ↓ ↓ ↓ 
+    if (phrase === "stop") {
+        console.log("red");
+    }
+    else if (phrase === "slow") {
+        console.log("yellow");
+    }
+    else if (phrase === "go") {
+        console.log("green");
+    }
+    else {
+        console.log("purple");
+    }
+}
