54cd5cb75fa228c59dac89b1d35be98201f7491e
MatiDev <mtsz.raszeja@gmail.com>
2024-02-27 12:50:11 +0100

    L03 - Arraye DONE

--- L03.js
+++ L03.js
@@ -103,6 +103,29 @@ let pelnyTydzien = dniPracujace.concat(dniWeekendowe);
 
 dniPracujace.reverse();
 
+const tictactoe = [
+    ["X", "X", 0],
+    [0, null, "X"],
+    [0, "X", "X"]
+];
+
+tictactoe[1][1];
+
+// DO NOT TOUCH!!! (please)
+const airplaneSeats = [
+    ['Ruth', 'Anthony', 'Stevie'],
+    ['Amelia', 'Pedro', 'Maya'],
+    ['Xavier', 'Ananya', 'Luis'],
+    ['Luke', null, 'Deniz'],
+    ['Rin', 'Sakura', 'Francisco']
+];
+
+// YOUR CODE GOES BELOW THIS LINE:
+
+airplaneSeats[3][1] = 'Hugo'
+
+
+
 
 
 
