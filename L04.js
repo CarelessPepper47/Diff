40cbcc6ea28eb77fae504c5aa6cdd3b931588a7e
MatiDev <mtsz.raszeja@gmail.com>
2024-02-28 11:21:55 +0100

    L04 - Objects DONE

--- L04.js
+++ L04.js
@@ -30,3 +30,41 @@ uzytkownicy.push({
     but: 44,
     log: false
 })
+
+//PLEASE DON'T TOUCH THIS LINE!
+const restaurant = {
+    name: 'Ichiran Ramen',
+    address: `${Math.floor(Math.random() * 100) + 1} Johnson Ave`,
+    city: 'Brooklyn',
+    state: 'NY',
+    zipcode: '11206',
+}
+
+//YOUR CODE GOES DOWN HERE:
+
+let fullAddress1 = `${restaurant.address}, ${restaurant.city}, state: ${restaurant.state} with zipcode ${restaurant.zipcode}`
+
+let fullAddress2 = `${restaurant["address"]}, ${restaurant["city"]}, ${restaurant["state"]}, ${restaurant["zipcode"]}`
+
+const midterms = {
+    Adam: 98,
+    Lol: 86,
+    Git: 69,
+    Goge: 24
+}
+
+midterms.Git = 55;
+
+midterms.NowyTyp = 12;
+
+const mateuszek = {
+    imie: "Mateusz",
+    nazwisko: "Raszeja",
+    styl: ["HTML", "Tailwindcss", "JS", "React"],
+    proporcje: {
+        wzrost: 184,
+        waga: 75,
+        stopa: 45
+    }
+}
+
