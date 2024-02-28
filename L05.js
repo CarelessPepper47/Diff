3dc3ad07e8e2d02f203f5f3462d6e0fc01774cfa
MatiDev <mtsz.raszeja@gmail.com>
2024-02-28 17:18:27 +0100

    L05 - Nested Loops

---
+++ L05.js
@@ -0,0 +1,50 @@
+for (let i = 1; i <= 10; i++) {
+    console.log("dupa");
+}
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i);
+// }
+
+// Print out "Da ba dee da ba daa" 6 times, using a for loop
+
+// for (let i = 1; i <= 6; i++) {
+//     console.log("Da ba dee da ba daa");
+// }
+
+// for (let i = 0; i <= 20; i += 2) {
+//     console.log(i);
+// }
+
+// for (let i = 100; i >= 0; i -= 10) {
+//     console.log(i);
+// }
+
+for (let i = 2; i <= 50; i *= 2) {
+    console.log(i)
+}
+
+let tydzien = ["Poniedziałek", "Wtorek", "Środa", "Czwartek", "Piątek", "Sobota", "Niedziela"]
+
+for (let i = 0; i < tydzien.length; i++) {
+    console.log(i, tydzien[i])
+}
+
+for (let i = tydzien.length-1; i >= 0; i--) {
+    console.log(i, tydzien[i])
+}
+
+const people = ["Scooby", "Velma", "Daphne", "Shaggy", "Fred"]; 
+
+for (let i = 0; i < people.length; i++) {
+    console.log(people[i].toUpperCase())
+}
+
+let przyklad = "Z dupy";
+
+for (i = 0; i <= 5; i++) {
+    console.log("Outer: Gówno", i);
+    for (j = 0; j < przyklad.length; j++) {
+        console.log("Inner:", przyklad[j])
+    }
+}
