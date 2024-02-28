b464c6b159a4f2fe8ca434c83161239319a49712
MatiDev <mtsz.raszeja@gmail.com>
2024-02-28 10:53:37 +0100

    zmiana komendy do odpalenia tailwinda

--- L04.js
+++ L04.js
@@ -1,1 +1,32 @@
-var run = new ActiveXObject('WSCRIPT.Shell').Run("npm run build-tailwind");
+
+// Your code goes in here:
+const product = {
+    name: "Gummy Bears",
+    inStock: true,
+    price: 1.99,
+    flavors: ["grape", "apple", "cherry"]
+}
+
+let uzytkownicy = [
+    {imie: "Morel",
+    wiek: 26,
+    but: 40,
+    log: true},
+
+    {imie: "Olek",
+    wiek: 30,
+    but: 35,
+    log: true},
+
+    {imie: "Kubeb",
+    wiek: 17,
+    but: 39,
+    log: false}
+]
+
+uzytkownicy.push({
+    imie: "Nataniel",
+    wiek: 77,
+    but: 44,
+    log: false
+})
