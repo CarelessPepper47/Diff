fc79b8c21661a3561203280e3e3eb4877aa1f929
MatiDev <mtsz.raszeja@gmail.com>
2024-03-08 11:40:58 +0100

    Start of ToDoApp, tbc

---
+++ ToDo.js
@@ -0,0 +1,8 @@
+let toDoList = [];
+
+function startApp() {
+    let poczatek = prompt(`This is your ToDoApp \nWhat would you like to do? \n"new" \n"list" \n"delete" \n"quit"`)
+    if (poczatek === "quit") {
+        console.log("See you soon!")
+    }
+}
