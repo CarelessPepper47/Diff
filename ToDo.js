10145c3122e8711b81514c84c0bde16e0e35c36b
MatiDev <mtsz.raszeja@gmail.com>
2024-03-09 09:26:20 +0100

    ToDoApp DONE; teraz zobaczymy jak to szefuncio zrobil

--- ToDo.js
+++ ToDo.js
@@ -8,6 +8,8 @@ function startApp() {
 
     dziala = true;
 
+    let usun;
+
     while (dziala) {
         if (poczatek === "new") {
             let newToDo = prompt("Make a new To Do")
@@ -20,7 +22,21 @@ function startApp() {
             }
             poczatek = prompt(`This is your ToDoApp \nWhat would you like to do? \n"new" \n"list" \n"delete" \n"quit"`)
         } else if (poczatek === "delete") {
+            usun = parseInt(prompt(`What to delete?`))
+            if (usun >= 1 && usun <= toDoList.length) {
+                console.log(`You deleted ${toDoList[usun-1]}`)
+                toDoList.splice(usun - 1, 1)
+                poczatek = prompt(`This is your ToDoApp \nWhat would you like to do? \n"new" \n"list" \n"delete" \n"quit"`)
+            } else {
+                console.log("Invalid choice!");
+                poczatek = prompt(`This is your ToDoApp \nWhat would you like to do? \n"new" \n"list" \n"delete" \n"quit"`)
+            }
             
+        } else if (poczatek === "quit") {
+            console.log(`You left ToDoApp!`)
+            break;
+        } else {
+            poczatek = prompt(`You must type a valid command. \n"quit" to quit an app`)
         }
     }
 }
