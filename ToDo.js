66cbb92c8b7118d0c04895d2a432ae1dcf3db979
MatiDev <mtsz.raszeja@gmail.com>
2024-03-09 11:06:34 +0100

    ToDo DONE: poprawione

--- ToDo.js
+++ ToDo.js
@@ -13,26 +13,23 @@ function startApp() {
             let newToDo = prompt("Make a new To Do")
             toDoList.push(newToDo)
             console.log(`Dodano do listy ${newToDo}`)
-            poczatek = prompt(`This is your ToDoApp \nWhat would you like to do? \n"new" \n"list" \n"delete" \n"quit"`)
         } else if (poczatek === "list") {
             for (let rzecz of toDoList) {
                 console.log(`${toDoList.indexOf(rzecz) + 1}. ${rzecz}`)
             }
-            poczatek = prompt(`This is your ToDoApp \nWhat would you like to do? \n"new" \n"list" \n"delete" \n"quit"`)
         } else if (poczatek === "delete") {
             usun = parseInt(prompt(`What to delete?`))
             if (usun >= 1 && usun <= toDoList.length) {
                 console.log(`You deleted ${toDoList[usun-1]}`)
                 toDoList.splice(usun - 1, 1)
-                poczatek = prompt(`This is your ToDoApp \nWhat would you like to do? \n"new" \n"list" \n"delete" \n"quit"`)
             } else {
                 console.log("Invalid choice!");
-                poczatek = prompt(`This is your ToDoApp \nWhat would you like to do? \n"new" \n"list" \n"delete" \n"quit"`)
             }
             
         } else {
             poczatek = prompt(`You must type a valid command.\n"new" \n"list" \n"delete" \n"quit"`)
         }
+        poczatek = prompt(`This is your ToDoApp \nWhat would you like to do? \n"new" \n"list" \n"delete" \n"quit"`)
     }
     console.log("You left ToDoApp!")
 }
