e598c80eaf93420ca498e0d0f0fc673cb86d0167
MatiDev <mtsz.raszeja@gmail.com>
2024-03-10 20:54:15 +0100

    Arguments in functions

--- L06.js
+++ L06.js
@@ -8,3 +8,42 @@ function app() {
 function updateDice() {
     document.getElementById("roll").innerText = die;
 }
+
+// Write your function here:
+
+function printHeart() {
+    console.log("<3");
+}
+
+printHeart();
+
+let sidesNumber;
+
+function giveNumberForDie() {
+    if (!sidesNumber) {
+        sidesNumber = parseInt(prompt("How many sides?"))
+    }
+    updateBetterDie();
+    rollABetterDie()
+}
+
+function reset() {
+    sidesNumber = null;
+    document.getElementById("sides").innerText = 0
+    document.getElementById("rolled").innerText = 0
+}
+
+let betterDie;
+
+function rollABetterDie() {
+    betterDie = Math.floor(Math.random() * sidesNumber) + 1
+    rolledBetterDie()
+}
+
+function updateBetterDie() {
+    document.getElementById("sides").innerText = sidesNumber;
+}
+function rolledBetterDie() {
+    document.getElementById("rolled").innerText = betterDie;
+}
+
