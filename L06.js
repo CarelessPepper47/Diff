7c7e9cea1fbf56d8dbdbd3dacdc052cc7f40ebee
MatiDev <mtsz.raszeja@gmail.com>
2024-03-11 09:08:52 +0100

    more

--- L06.js
+++ L06.js
@@ -24,7 +24,7 @@ function giveNumberForDie() {
         sidesNumber = parseInt(prompt("How many sides?"))
     }
     updateBetterDie();
-    rollABetterDie()
+    rollABetterDie();
 }
 
 function reset() {
@@ -59,7 +59,7 @@ function repeat(str, y) {
     for (let i = 0; i < y; i++) {
         console.log(str)
     }
-    
+
     let repetition = "";
     for (let i = 0; i < y; i++) {
         repetition += str;
@@ -68,4 +68,39 @@ function repeat(str, y) {
 }
 
 repeat(str, 3)
+
+// define isSnakeEyes below:
+
+function isSnakeEyes(side1, side2) {
+    Math.floor(Math.random() * side1) +1;
+    Math.floor(Math.random() * side2) +1;
+    
+    if (side1 === 1 && side2 === 1) {
+        console.log(`Snake Eyes!`)
+    }
+    
+    else {
+        console.log("Not Snake Eyes!")
+    }
+    
+}
+
+isSnakeEyes(6, 6)
+
+function add(x, y) {
+    if (typeof x !== 'number' || typeof y !== 'number') {
+        return false;
+    }
+    let sum = x + y
+    return sum;
+}
+
+// DEFINE YOUR FUNCTION BELOW:
+
+function multiply(x,y) {
+    return x * y;
+}
+
+let mnozenie = multiply(x,y)
+
 
